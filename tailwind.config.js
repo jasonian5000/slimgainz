6de203d373f8caf24b554447d3637f1efe29d4bd
Jason <jason.blunck@gmail.com>
2023-01-16 18:53:15 -0600

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    require("@tailwindcss/forms"),
+  ],
+
+  daisyui: {
+    themes: ["dark"],
+  },
+};
